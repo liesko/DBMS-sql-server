e31ca8f76390530c769d6781b1cea9e8f6ea149f
Ivana Lieska <idrabikova@gmail.com>
2016-05-04 23:40:22 +0200

    object creator

---
+++ DBManagementSystem/DataHandler/ObjectCreator.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DBManagementSystem.Security;
+
+namespace DBManagementSystem.DataHandler
+{
+    public static class ObjectCreator
+    {
+        public static void CreateTable(NewConnection connection, string tableName)
+        {
+            
+        }
+
+        public static void CreateColumn(NewConnection connection, string columnName)
+        {
+            string commandString = "ALTER TABLE " + connection.ActualTable +" ADD " + columnName + " VARCHAR(20);";
+            Console.WriteLine(commandString);
+            SqlCommand sqlCmd = new SqlCommand(commandString, connection.Connection);
+            sqlCmd.ExecuteNonQuery();
+        }
+
+        public static void CreateDatabase(NewConnection connection, string databaseName)
+        {
+
+        }
+    }
+}
