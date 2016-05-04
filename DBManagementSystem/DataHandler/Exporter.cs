d4cda5b79c94e7cafb4bcb79b4de314a2a66e784
Ivana Lieska <idrabikova@gmail.com>
2016-05-04 22:12:05 +0200

    Exporting data to CSV

---
+++ DBManagementSystem/DataHandler/Exporter.cs
@@ -0,0 +1,48 @@
+﻿using System.Windows.Forms;
+using System.IO;
+
+namespace DBManagementSystem.DataHandler
+{
+    class Exporter
+    {
+        public static string FileName { get; set; }
+        public static string Path { get; set; }
+
+        public static void Export(DataGridView dataGridView)
+        {
+            //Build the CSV file data as a Comma separated string.
+            string csv = string.Empty;
+            DataGridView dataGridView1 = dataGridView;
+
+            //Add the Header row for CSV file.
+            foreach (DataGridViewColumn column in dataGridView1.Columns)
+            {
+                csv += column.HeaderText + ',';
+            }
+
+            //Add new line.
+            csv += "\r\n";
+
+            //Adding the Rows
+            foreach (DataGridViewRow row in dataGridView1.Rows)
+            {
+                foreach (DataGridViewCell cell in row.Cells)
+                {
+                    //Add the Data rows.
+                    if (cell.Value != null)
+                    {
+                        csv += cell.Value.ToString().Replace(",", ";") + ',';
+                    }
+
+                }
+
+                //Add new line.
+                csv += "\r\n";
+            }
+
+            //Exporting to CSV.
+            string folderPath = "C:\\CSV\\";
+            File.WriteAllText(folderPath + FileName +".csv", csv);
+        }
+    }
+}
---
+++ DBManagementSystem/View/ExportDialog.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DBManagementSystem.DataHandler;
+using DBManagementSystem.Security;
+
+namespace DBManagementSystem.View
+{
+    class ExportDialog : NewDialog
+    {
+        public override string ShowDialog(string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 500,
+                Height = 240,
+                FormBorderStyle = FormBorderStyle.FixedDialog,
+                Text = caption,
+                StartPosition = FormStartPosition.CenterScreen
+            };
+
+            string location = "Location";
+            string fileName = "File Name";
+
+            Label locationLabel = new Label() { Left = 50, Top = 20, Text = location };
+            TextBox locationText = new TextBox() { Left = 50, Top = 50, Width = 400 };
+
+            Label fileNameLabel = new Label() { Left = 50, Top = 80, Text = fileName };
+            TextBox fileNameText = new TextBox() { Left = 50, Top = 110, Width = 400 };
+
+            Button confirmation = new Button()
+            {
+                Text = "Export",
+                Left = 350,
+                Width = 100,
+                Top = 150,
+                DialogResult = DialogResult.OK
+            };
+
+            confirmation.Click += delegate {
+                Exporter.FileName = fileNameText.Text;
+                Exporter.Path = locationText.Text;
+            };
+
+            prompt.Controls.Add(locationText);
+            prompt.Controls.Add(locationLabel);
+            prompt.Controls.Add(fileNameText);
+            prompt.Controls.Add(fileNameLabel);
+
+            prompt.Controls.Add(confirmation);
+            prompt.AcceptButton = confirmation;
+
+            return prompt.ShowDialog() == DialogResult.OK ? "done" : "error";
+        }
+    }
+}
